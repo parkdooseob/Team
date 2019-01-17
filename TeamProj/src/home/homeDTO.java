b629ee03a65f19a8183c9175a5cadc172cd59f2d
eun89jin <eun89jin@naver.com>
2019-01-17 17:58:21 +0900

    homeDB

---
+++ TeamProj/src/home/homeDTO.java
@@ -0,0 +1,52 @@
+package home;
+
+public class homeDTO {
+	/**공간*/
+	
+	/*
+	 CREATE TABLE `hosting` (
+  `host_no` int(11) DEFAULT NULL COMMENT '호스트번호',외래키
+  `room_no` int(11) NOT NULL AUTO_INCREMENT COMMENT '룸자동생성번호',
+  `content` varchar(45) NOT NULL COMMENT '제목',
+  `post` varchar(45) NOT NULL COMMENT '비고',
+  `room` varchar(10) NOT NULL COMMENT '공간타입',
+  `people` varchar(45) NOT NULL COMMENT '수용인원수',
+  `title` varchar(45) NOT NULL,
+  `pDay` varchar(45) NOT NULL,
+  PRIMARY KEY (`room_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8
+	  */
+	
+	
+	/**사진*/
+	/*
+	
+CREATE TABLE `hosting_pic` (
+  `room_no` int(11) NOT NULL AUTO_INCREMENT COMMENT '룸자동생성번호',
+  `pic1` varchar(45) NOT NULL,
+  `pic2` varchar(45) NOT NULL,
+  `pic3` varchar(45) NOT NULL,
+  PRIMARY KEY (`room_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
+	*/
+	
+	/**호스트*/
+	/*
+	
+	
+	*/
+	
+	/*
+	 :
+	
+	*/
+	
+	
+	/**리뷰*/
+	/*
+	 리뷰
+	 */
+	
+	/**예약일*/
+	
+}
